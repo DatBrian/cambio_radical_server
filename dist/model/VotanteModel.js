e72f74da2df73688279ba95bf913b6f65028d835
DatBrian <vikard.2005@hotmail.com>
2023-08-14 11:42:09 -0500

    feat: :package: Se actualizaron los archivos compilados

--- dist/model/VotanteModel.js
+++ dist/model/VotanteModel.js
@@ -8,98 +8,79 @@ class VotanteSchema {
         this.schema = new mongoose_1.Schema({
             name: {
                 type: String,
-                required: true,
                 unique: true,
                 trim: true,
             },
             lider: {
                 type: String,
-                required: true,
                 trim: true,
             },
             doc: {
                 type: Number,
-                required: true,
                 unique: true,
                 trim: true,
             },
             genero: {
                 type: String,
-                required: true,
                 trim: true,
             },
             celular: {
                 type: Number,
-                required: true,
                 trim: true,
             },
             nacimiento: {
                 type: Date,
-                required: true,
                 trim: true,
             },
             telefono: {
                 type: Number,
-                required: true,
-                unique: true,
                 trim: true,
             },
             direccion: {
                 type: String,
-                required: true,
                 trim: true,
             },
             barrio: {
                 type: String,
-                required: true,
                 trim: true,
             },
             comuna: {
                 type: Number,
-                required: true,
                 trim: true,
             },
             email: {
                 type: String,
-                required: true,
                 trim: true,
             },
             profesion: {
                 type: String,
-                required: true,
                 trim: true,
             },
             ocupacion: {
                 type: String,
-                required: true,
                 trim: true,
             },
             RS: [
                 {
                     type: Object,
                     required: false,
-                    unique: true,
                     trim: true,
                 },
             ],
             PuestoVotacion: {
                 type: String,
-                required: true,
                 trim: true,
             },
             MesaVotacion: {
                 type: Number,
-                required: true,
                 trim: true,
             },
             compromiso: {
                 type: String,
-                required: true,
                 trim: true,
             },
             fidelidad: {
                 type: String,
-                required: true,
                 trim: true,
             },
         }, {
