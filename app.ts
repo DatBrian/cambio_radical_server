8f3ad645a85b2813530fd0e519a292138b53d4e2
DatBrian <vikard.2005@hotmail.com>
2023-08-30 13:10:18 -0500

    fix: :lock: Nuevos origenes de acceso
    Se agrego una lista blanca a los origenes deeee acceso con cors

--- app.ts
+++ app.ts
@@ -14,102 +14,111 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class App extends ConnectionDB {
-  public app: Application;
-  public port: number;
-  public server: any;
+    public app: Application;
+    public port: number;
+    public server: any;
+    private whiteList: any;
 
-  constructor(routes: RoutesInterface[]) {
-    super();
-    this.app = express();
-    this.port = Number(process.env.PORT) || 5000;
-    this.initMiddlewares();
-    this.initRoutes(routes);
-    this.initConnection();
-  }
-
-  public getServer() {
-    return this.app;
-  }
-
-  public closeServer(done?: any) {
-    this.server = this.app.listen(this.port, () => {
-      done();
-    });
-  }
-
-  private async initConnection() {
-    try {
-      const connection = await this.connect();
-      console.log(chalk.bgGreen.black("✔️  Conexión establecida 🔌 "));
-      console.log(
-        chalk.blue(
-          "---------------------------------------------------------------------------------"
-        )
-      );
-      console.log(
-        chalk.green.bold(`🌐 ¡Se ha establecido la conexión a: ${process.env.DB_NAME}!`)
-      );
-      console.log(
-        chalk.blue(
-          "---------------------------------------------------------------------------------"
-        )
-      );
-      return connection;
-    } catch (error) {
-      console.error(chalk.bgRed.white("❌ Error al establecer la conexión:"));
-      console.error(error);
-      throw new Error("Error al establecer la conexión");
+    constructor(routes: RoutesInterface[]) {
+        super();
+        this.app = express();
+        this.port = Number(process.env.PORT) || 5000;
+        this.initMiddlewares();
+        this.initRoutes(routes);
+        this.initConnection();
+        this.whiteList = [
+            "https://cambioradicalserver.up.railway.app/",
+            "http://localhost:8080",
+        ];
     }
-  }
 
-  private initMiddlewares() {
-    this.app.use(express.json());
-    this.app.use(cors());
-    this.app.use(morgan("dev"));
-    this.app.use(
-      session({
-        secret: "process.env.JWT_PRIVATE_KEY",
-        resave: false,
-        saveUninitialized: false,
-      })
-    );
-    this.app.use(passport.initialize());
-    this.app.use(passport.session());
-  }
+    public getServer() {
+        return this.app;
+    }
 
-  private initRoutes(routes: RoutesInterface[]) {
-    this.app.use(`/api/${process.env.API_VERSION}`, authRoutes.router);
-    routes.forEach((route) => {
-      this.app.use(
-        `/api/${process.env.API_VERSION}`,
-        authJWTMiddleware.validateToken,
-        route.router
-      );
-    });
-  }
+    public closeServer(done?: any) {
+        this.server = this.app.listen(this.port, () => {
+            done();
+        });
+    }
 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log();
-      console.log(chalk.bgCyan.white.bold("🗺️  Rutas disponibles: 🚴 "));
-      routemap(this.app);
-      console.log(chalk.bgGreen.black("✨ Servidor en línea ✨ "));
-      console.log(
-        chalk.blue(
-          "--------------------------------------------------------------------------------"
-        )
-      );
-      console.log(
-        chalk.green.bold(
-          `🚀 ¡El servidor se ha levantado exitosamente en http://${process.env.HOST}:${process.env.PORT}!`
-        )
-      );
-      console.log(
-        chalk.blue(
-          "--------------------------------------------------------------------------------"
-        )
-      );
-    });
-  }
+    private async initConnection() {
+        try {
+            const connection = await this.connect();
+            console.log(chalk.bgGreen.black("✔️  Conexión establecida 🔌 "));
+            console.log(
+                chalk.blue(
+                    "---------------------------------------------------------------------------------"
+                )
+            );
+            console.log(
+                chalk.green.bold(
+                    `🌐 ¡Se ha establecido la conexión a: ${process.env.DB_NAME}!`
+                )
+            );
+            console.log(
+                chalk.blue(
+                    "---------------------------------------------------------------------------------"
+                )
+            );
+            return connection;
+        } catch (error) {
+            console.error(
+                chalk.bgRed.white("❌ Error al establecer la conexión:")
+            );
+            console.error(error);
+            throw new Error("Error al establecer la conexión");
+        }
+    }
+
+    private initMiddlewares() {
+        this.app.use(express.json());
+        this.app.use(cors({ origin: this.whiteList }));
+        this.app.use(morgan("dev"));
+        this.app.use(
+            session({
+                secret: "process.env.JWT_PRIVATE_KEY",
+                resave: false,
+                saveUninitialized: false,
+            })
+        );
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+    }
+
+    private initRoutes(routes: RoutesInterface[]) {
+        this.app.use(`/api/${process.env.API_VERSION}`, authRoutes.router);
+        routes.forEach((route) => {
+            this.app.use(
+                `/api/${process.env.API_VERSION}`,
+                authJWTMiddleware.validateToken,
+                route.router
+            );
+        });
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log();
+            console.log(chalk.bgCyan.white.bold("🗺️  Rutas disponibles: 🚴 "));
+            routemap(this.app);
+            console.log(chalk.bgGreen.black("✨ Servidor en línea ✨ "));
+            console.log(
+                chalk.blue(
+                    "--------------------------------------------------------------------------------"
+                )
+            );
+            console.log(
+                chalk.green.bold(
+                    `🚀 ¡El servidor se ha levantado exitosamente en http://${process.env.HOST}:${process.env.PORT}!`
+                )
+            );
+            console.log(
+                chalk.blue(
+                    "--------------------------------------------------------------------------------"
+                )
+            );
+        });
+    }
 }
 export default App;
